c80f019c638486ac1c9f5b6856a60093de738913
Dean Stirrat <deanstirrat@gmail.com>
2022-02-16 20:37:11 -0500

    template file uploaded

---
+++ The Kitchen App/The Kitchen App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Kitchen App
+//
+//  Created by Dean Stirrat on 2/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Kitchen App/The Kitchen App/The_Kitchen_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Kitchen_AppApp.swift
+//  The Kitchen App
+//
+//  Created by Dean Stirrat on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct The_Kitchen_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
